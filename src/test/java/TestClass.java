a760404ccd6bd6404a63e33995659e91f424ccbe
Anton_Tsymbal <anton_tsymbal@epam.com>
2016-10-06 11:02:31 +0300

    Code refactored.

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,21 +1,12 @@
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
 
-@RunWith(MockitoJUnitRunner.class)
 public class TestClass {
     private static final double delta = 0.000001;
 
